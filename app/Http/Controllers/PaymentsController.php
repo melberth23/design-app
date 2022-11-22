8fcbd153bb2cf7eb9e2f4e9208b8108a1c8f4372
melberth23 <melberthbontilao@gmail.com>
2022-11-23 01:41:07 +0800

    Free upgrade issue

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -147,25 +147,75 @@ class PaymentsController extends Controller
             $rowpayment = NewAttempt::where('reference', $payments['reference'])->first();
             $user = User::whereId($rowpayment->user_id)->first();
 
-            // Cancel old subscription
-            // Get Payment Config
-            $apikey = config('services.hitpay.key');
-            $isStg = config('services.hitpay.environment');
-            $paymentApi = new PaymentHelper($apikey, $isStg);
-            $responseApi = $paymentApi->recurringDeleteAccount($user->payments->reference);
-            if(!empty($responseApi['status']) && $responseApi['status'] == 'canceled') {
-                $customerfullname = $user->first_name .' '. $user->last_name;
+            $helper = new SystemHelper();
+            $planInfo = $helper->getPlanInformation($rowpayment->plan);
+            
+            $customerfullname = $user->first_name .' '. $user->last_name;
+            $datetoday = date('Y-m-d', strtotime($user->payments->recurring_date));
 
-                $datetoday = date('Y-m-d', strtotime($user->payments->recurring_date));
-                // $current_date = strtotime($datetoday);
-                // $next_recurring_date = date("Y-m-d", strtotime("+1 month", $current_date));
+            if( 'free' !== $user->payments->plan ) {
+                // Cancel old subscription
+                // Get Payment Config
+                $apikey = config('services.hitpay.key');
+                $isStg = config('services.hitpay.environment');
+                $paymentApi = new PaymentHelper($apikey, $isStg);
+                $responseApi = $paymentApi->recurringDeleteAccount($user->payments->reference);
+                if(!empty($responseApi['status']) && $responseApi['status'] == 'canceled') {
 
-                // Cancel old payment
-                Payments::whereId($user->payments->id)->update(['status' => 'cancelled']);
+                    // Cancel old payment
+                    Payments::whereId($user->payments->id)->update(['status' => 'cancelled']);
 
-                $helper = new SystemHelper();
-                $planInfo = $helper->getPlanInformation($rowpayment->plan);
+                    // Save new payment
+                    $payment = Payments::create([
+                        'user_id' => $user->id,
+                        'reference' => $rowpayment->reference,
+                        'business_recurring_plans_id' => $rowpayment->business_recurring_plans_id,
+                        'plan' => $rowpayment->plan,
+                        'cycle' => $rowpayment->cycle,
+                        'currency' => $rowpayment->currency,
+                        'price' => $rowpayment->price,
+                        'status' => $payments['status'],
+                        'payment_methods' => $rowpayment->payment_methods,
+                        'payment_url' => $rowpayment->payment_url,
+                        'type' => $payments['type'],
+                        'duration' => $rowpayment->duration
+                    ]);
+                    Payments::whereId($payment->id)->update(['recurring_date' => $datetoday]);
 
+                    // Create invoice
+                    $last_invoice = Invoices::where('user_id', $user->id)->latest('created_at')->first();
+                    $invoice_number = 100000000;
+                    if(!empty($last_invoice)) {
+                        $invoice_number = intval($last_invoice->number) + 1;
+                    }
+                    Invoices::create([
+                        'user_id' => $user->id,
+                        'payment_id' => $payment->id,
+                        'number' => $invoice_number,
+                        'date_invoice' => date('Y-m-d'),
+                        'plan' => $payment->plan,
+                        'amount' => $payment->price
+                    ]);
+
+                    // Send confirmation email
+                    $details = array(
+                        'subject' => 'Your account is upgraded',
+                        'message' => 'Greetings '. $customerfullname,
+                        'extra_msg' => 'Please refer your plan information below:',
+                        'plan' => $planInfo['label'],
+                        'amount' => number_format($planInfo['amount']),
+                        'paymentlink' => '',
+                        'thank_msg' => 'Please login using your login information to check your account information. Thank you!',
+                        'template' => 'payment'
+                    );
+                    Mail::to($user->email)->send(new DigitalMail($details));
+
+                    #Commit Transaction
+                    DB::commit();
+
+                    $request->session()->flash('success', 'Your account successfully upgraded!');
+                }
+            } else {
                 // Save new payment
                 $payment = Payments::create([
                     'user_id' => $user->id,
